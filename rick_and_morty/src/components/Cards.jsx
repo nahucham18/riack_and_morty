b8e5de0c1f97c07b01857d197c69b503734cefb6
nahucham18 <nahu.chamo19@gmail.com>
2023-03-02 09:31:27 -0300

    01 Integracion

---
+++ rick_and_morty/src/components/Cards.jsx
@@ -0,0 +1,22 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+
+   return ( 
+   <div>
+      {
+         characters?.map((char) => ( 
+               <Card   name={char.name}
+                           species={char.species}
+                           gender={char.gender}
+                           image={char.image}
+                           onClose={() => window.alert('Emulamos que se cierra la card')}
+                           key={char.id}
+                           />
+         ))}  
+      
+   </div>
+         )
+}
+
